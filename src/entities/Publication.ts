10c0397385d7e3cd429ef43909e5fb23f4159e5b
Adrian Kurek <adrian.kurek93@gmail.com>
2021-03-25 22:31:58 +0100

    Update

--- src/entities/Publication.ts
+++ src/entities/Publication.ts
@@ -1,7 +1,35 @@
 export class Publication {
-    constructor(public title: string, public date: string, public userName: String) {
-        this.title = title;
+    private _title: string;
+    private _date: string;
+    private _userName: string;
+
+    constructor(title: string, date: string, userName: string) {
+        this._title = title;
+        this._date = date;
+        this._userName = userName;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title: string) {
+        this._title = title;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    set date(date: string) {
         this.date = date;
-        this.userName = userName;
+    }
+
+    get userName() {
+        return this._userName;
+    }
+
+    set userName(userName: string) {
+        this._userName = userName;
     }
 }
