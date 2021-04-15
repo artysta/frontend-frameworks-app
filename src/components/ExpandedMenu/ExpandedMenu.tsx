54a26e2553ef52ef059ac37babdb99ee86120d6b
Adrian Kurek <adrian.kurek93@gmail.com>
2021-04-15 20:39:26 +0200

    Add some styles for dropdown menu

--- src/components/ExpandedMenu/ExpandedMenu.tsx
+++ src/components/ExpandedMenu/ExpandedMenu.tsx
@@ -11,28 +11,68 @@ const Wrapper = styled.div`
   border-width: 0 1px 1px 1px;
   padding: 10px;
   margin: 25px 0 0 0;
+  width: auto;
+
+  ul {
+    li {
+      padding: 7px;
+      display: flex;
+    }
+  }
+
+  img {
+    margin-right: 15px;
+  }
 `;
+
+const SectionTitle = styled.span`
+  font-size: 15px;
+`
+
+const FilterWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+
+  input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #c7c7c7;
+    border-radius: 5px;
+    outline: none;
+  }
+`
 
 export const ExpandedMenu: FC = () => {
   return (
     <Wrapper>
-      <input type="text" placeholder="Filter..." />
+      <FilterWrapper>
+        <input type="text" placeholder="Filter..."></input>  
+      </FilterWrapper>
       <br />
-      <span>Platform</span>
+      <SectionTitle>Platform</SectionTitle>
       <ul>
-        <li>Home</li>
-        <li>Publications</li>
-        <li>People</li>
-        <li>Entities</li>
-        <li>Administration</li>
+        <li><img src="./media/icons/house.svg" />Home</li>
+        <li><img src="./media/icons/publications.svg" />Publications</li>
+        <li><img src="./media/icons/people.svg" />People</li>
+        <li><img src="./media/icons/entities.svg" />Entities</li>
+        <li><img src="./media/icons/administration.svg" />Administration</li>
       </ul>
-      <span>Workspaces</span>
+      <SectionTitle>Workspaces</SectionTitle>
       <ul>
-        <li>Client contract</li>
-        <li>Supplier contract</li>
-        <li>Corporate</li>
-        <li>Group Norms</li>
-        <li>Real estate contracts</li>
+        <li><img src="./media/icons/entities2.svg" />Client contract</li>
+        <li><img src="./media/icons/house2.svg" />Supplier contract</li>
+        <li><img src="./media/icons/entities.svg" />Corporate</li>
+        <li><img src="./media/icons/house.svg" />Group Norms</li>
+        <li><img src="./media/icons/house.svg" />Real estate contracts</li>
+      </ul>
+      <SectionTitle>Account</SectionTitle>
+      <ul>
+        <li><img src="./media/icons/house.svg" />User name</li>
+        <li><img src="./media/icons/privacy.svg" />Privacy</li>
+        <li><img src="./media/icons/settings.svg" />Settings</li>
       </ul>
     </Wrapper>
   );
