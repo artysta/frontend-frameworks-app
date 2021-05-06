d287e7bc9a28d41a1b2b1c2b2f84d0f71d35adc1
Adrian Kurek <adrian.kurek93@gmail.com>
2021-05-06 18:29:59 +0200

    Add some new colors

--- src/styledHelpers/Colors.ts
+++ src/styledHelpers/Colors.ts
@@ -1,4 +1,11 @@
 export const Colors = {
     white: '#FFF',
-    black: '#000'
+    black: '#000',
+    gray1: '#a0a2a7',
+    gray2: '#c7c7c7',
+    gray3: '#e8e8e8',
+    gray4: '#ddd',
+    blue1: '#4b5268',
+    blue2: '#2f439f',
+    blue3: '#5783e9',
 }
