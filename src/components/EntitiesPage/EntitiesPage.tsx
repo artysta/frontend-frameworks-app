dcfee977968827ff8aafe59028640b35d6d36f90
Adrian Kurek <adrian.kurek93@gmail.com>
2021-05-13 20:02:44 +0200

    Add page header and display entities

--- src/components/EntitiesPage/EntitiesPage.tsx
+++ src/components/EntitiesPage/EntitiesPage.tsx
@@ -1,23 +1,117 @@
 import { FC } from "react";
 import styled from "styled-components";
-import { Colors } from "../../styledHelpers/Colors"
+import { Colors } from "../../styledHelpers/Colors";
+import { Workspace } from "../../entities/Workspace";
+import { FakeWorkspacesRepository } from "../../repositories/FakeWorkspacesRepository";
+import { Repository } from "../../repositories/Repository";
 
 const Wrapper = styled.div`
-  margin: 20px;
-  padding: 20px;
-  border-radius: 5px;
   width: 100%;
-  box-shadow: 0px 1px 3px ${Colors.gray2};
-  justify-content: center;
+  margin: 20px;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 100%;
   display: flex;
+`;
+
+const Header = styled.span`
+  color: ${Colors.blue3};
+  font-size: 20px;
+  margin: auto 0;
+`;
+
+const CardsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 15px;
+  margin-top: 20px;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 `;
 
+const Card = styled.div`
+  display: flex;
+  box-shadow: 0px 1px 3px ${Colors.gray2};
+  padding: 20px;
+  border-radius: 5px;
+
+  div {
+    display: block;
+  }
+`;
+
+const Title = styled.p`
+  color: ${Colors.blue3};
+  font-size: 20px;
+`;
+
+const CardDetails = styled.div`
+  display: table-cell;
+  position: relative;
+  padding: 10px;
+  width: 100%;
+`;
+
+const CardFooter = styled.div`
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 5px;
+`;
+
+const Select = styled.select`
+  width: 200px;
+  padding: 10px;
+  border: 0;
+  border-radius: 5px;
+  outline: none;
+  cursor: pointer;
+  color: ${Colors.blue3};
+  margin-left: auto;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  margin: auto 10px;
+`;
+
+function workspaces(): Workspace[] {
+  let data: Repository<Workspace> = new FakeWorkspacesRepository();
+  return data.getAll();
+}
+
 export const EntitiesPage: FC = () => {
   return (
     <Wrapper>
-      <span>This is a entities page!</span>
+      <HeaderWrapper>
+        <Header>Entities</Header>
+        <Icon src="../media/icons/people.svg"></Icon>
+        <Select>
+          <option>Mosaic</option>
+          <option>List</option>
+        </Select>
+      </HeaderWrapper>
+      <CardsWrapper>
+        {workspaces().map((w, i) => (
+          <Card>
+            <Image src="./placeholder350.png" />
+            <CardDetails>
+              <Title>{w.title}</Title>
+              <CardFooter>
+                <p>
+                  {w.type} &bull; {w.numberOfUsers} &bull; {w.lastUpdated}
+                </p>
+              </CardFooter>
+            </CardDetails>
+          </Card>
+        ))}
+      </CardsWrapper>
     </Wrapper>
   );
 };
