75a4bd624374c3da15423e965b600f942cd7670d
Adrian Kurek <adrian.kurek93@gmail.com>
2021-05-18 21:10:33 +0200

    Add Redux types and actions for user

---
+++ src/actions/userActions.ts
@@ -0,0 +1,21 @@
+import { Dispatch } from 'redux';
+import * as actionTypes from './actionTypes/userTypes';
+import { ISingleUser } from '../entities/ISingleUser';
+
+export const getUsers = (): Promise<ISingleUser[]> => ((dispatch: Dispatch) => {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+        .then(reponse => reponse.json())
+        .then((usersList: ISingleUser[]) => {
+            dispatch({
+                type: actionTypes.GET_USERS,
+                usersList
+            })
+        })
+}) as any;
+
+export const getSomeData = (someData: string): Promise<ISingleUser[]> => ((dispatch: Dispatch) => {
+    dispatch({
+        type: actionTypes.PUSH_DATA,
+        someData
+    })
+}) as any;
