3555c7315dbf03043d9785af3d4bbbe7fd317d5b
Adrian Kurek <adrian.kurek93@gmail.com>
2021-06-10 07:05:56 +0200

    Add more colors

--- src/styledHelpers/Colors.ts
+++ src/styledHelpers/Colors.ts
@@ -8,6 +8,9 @@ export const Colors = {
     blue1: '#4b5268',
     blue2: '#2f439f',
     blue3: '#5783e9',
+    blue4: '#eaecf5',
+    blue5: '#f5f7f9',
+    blue6: '#2a3f9d',
     aqua1: '#e6f0f3',
-    aqua2: '#549bae',
+    aqua2: '#549bae'
 }
