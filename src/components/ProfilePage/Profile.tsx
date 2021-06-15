d4b90b594aff2f23da27aef756f44de223ae09a2
Adrian Kurek <adrian.kurek93@gmail.com>
2021-06-15 18:05:09 +0200

    Update style of editable elements

--- src/components/ProfilePage/Profile.tsx
+++ src/components/ProfilePage/Profile.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import styled from "styled-components";
-import { Colors } from "../../styledHelpers/Colors"
+import { Colors } from "../../styledHelpers/Colors";
 
 const Wrapper = styled.div`
   display: flex;
@@ -14,53 +14,58 @@ const UserPhoto = styled.div`
   img {
     margin: 10px;
     width: 100px;
-    border-radius: 50%
+    border-radius: 50%;
   }
 `;
 
 const UserInformation = styled.div`
   width: 100%;
+  margin: 10px 0;
 `;
 
 const UserContact = styled.div`
   width: auto;
   display: flex;
-  align-items: flex-end;
+  float: right;
 `;
 
 const EditableField = styled.input`
   border: 0;
   outline: none;
   margin: 2px;
+  transition: 0.2s;
 
   :enabled {
     border: 1px solid ${Colors.blue3};
     border-radius: 3px;
+    transition: 0.2s;
 
     :focus {
-
+      border: 1px solid ${Colors.black};
+      background-color: ${Colors.aqua1};
+      transition: 0.2s;
     }
   }
 
   :disabled {
     background-color: ${Colors.white};
   }
-
-`
+`;
 
 const Icon = styled.img`
   width: 20px;
   height: 20px;
+
+  float: right;
 
   :hover {
     cursor: pointer;
   }
 `;
 
-
 export const Profile: FC = () => {
   const [editable, setEnabled] = useState(true);
-  
+
   const handleEdit = () => {
     setEnabled(function changeValue(value) {
       return !value;
@@ -74,23 +79,48 @@ export const Profile: FC = () => {
         <p>See profile</p>
       </UserPhoto>
       <UserInformation>
-        <EditableField disabled={editable} defaultValue="Humberta Swift" contentEditable={true}/>
+        <EditableField
+          disabled={editable}
+          defaultValue="Humberta Swift"
+          contentEditable={true}
+        />
+        <Icon src="./media/icons/settings.svg" onClick={handleEdit}></Icon>
         <br />
-        <EditableField disabled={editable} defaultValue="Clifford Chance" contentEditable={true}/>
+        <EditableField
+          disabled={editable}
+          defaultValue="Clifford Chance"
+          contentEditable={true}
+        />
         <br />
-        <EditableField disabled={editable} defaultValue="New York" contentEditable={true}/>
-          <br />
-        <EditableField disabled={editable} defaultValue="Parnert" contentEditable={true}/>
+        <EditableField
+          disabled={editable}
+          defaultValue="New York"
+          contentEditable={true}
+        />
         <br />
-      <Icon src="./media/icons/settings.svg" onClick={handleEdit}></Icon>
+        <EditableField
+          disabled={editable}
+          defaultValue="Parnert"
+          contentEditable={true}
+        />
+        <br />
+        <UserContact>
+          <p>
+            <EditableField
+              disabled={editable}
+              defaultValue="humbertaswift@mail.com"
+              contentEditable={true}
+            />
+
+            <br />
+            <EditableField
+              disabled={editable}
+              defaultValue="+33 (0) 6 12 34 56 78"
+              contentEditable={true}
+            />
+          </p>
+        </UserContact>
       </UserInformation>
-      <UserContact>
-        <p>
-          humbertaswift@mail.com
-          <br />
-          +33 (0) 6 12 34 56 78
-        </p>
-      </UserContact>
     </Wrapper>
   );
 };
