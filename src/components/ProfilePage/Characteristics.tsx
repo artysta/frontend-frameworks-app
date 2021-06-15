f5c97d42dc82c34636f46a36a7e49a72ffb23e85
Adrian Kurek <adrian.kurek93@gmail.com>
2021-06-15 18:27:30 +0200

    Allow to edit tag elements

--- src/components/ProfilePage/Characteristics.tsx
+++ src/components/ProfilePage/Characteristics.tsx
@@ -1,44 +1,115 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
 import { Colors } from "../../styledHelpers/Colors";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  border-top: solid 1px ${Colors.gray3};
+  padding-top: 10px;
+`;
 
 const Title = styled.p`
   color: ${Colors.gray1};
 `;
 
-const Tag = styled.button`
+const Tag = styled.select`
   background-color: ${Colors.aqua1};
   color: ${Colors.aqua2};
   border: 0;
-  padding: 3px 10px;
+  padding: 5px 10px;
   margin: 10px;
   border-radius: 5px;
+  transition: 0.2s;
+
+  :enabled {
+    color: ${Colors.btnBlue2};
+    background-color: ${Colors.btnBlue1};
+    transition: 0.2s;
+
+    :hover {
+      background-color: ${Colors.blue3};
+      color: ${Colors.white};
+      transition: 0.2s;
+    }
+  }
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+  float: right;
+
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 const TagsContainer = styled.div``;
 
 export const Characteristics: FC = () => {
+  const [editable, setEnabled] = useState(true);
+
+  const handleEdit = () => {
+    setEnabled(function changeValue(value) {
+      return !value;
+    });
+  };
+
   return (
     <Wrapper>
       <TagsContainer>
+        <Icon src="./media/icons/settings.svg" onClick={handleEdit}></Icon>
         <Title>Expertise</Title>
-        <Tag>Mergers and acquisition</Tag>
+        <Tag disabled={editable}>
+          <option>Mergers and acquisition</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
+          <option>Option 5</option>
+        </Tag>
       </TagsContainer>
       <TagsContainer>
         <Title>Specialties</Title>
-        <Tag>Cross border operation</Tag>
-        <Tag>Transaction over 500M€/$</Tag>
+        <Tag disabled={editable}>
+          <option>Cross border operation</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
+          <option>Option 4</option>
+        </Tag>
+        <Tag disabled={editable}>
+          <option>Transaction over 500M€/$</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
+          <option>Option 5</option>
+        </Tag>
       </TagsContainer>
       <TagsContainer>
         <Title>Admission to practice law</Title>
-        <Tag>Paris bar association</Tag>
-        <Tag>Tunissian bar association</Tag>
+        <Tag disabled={editable}>
+          <option>Paris bar association</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
+          <option>Option 5</option>
+        </Tag>
+        <Tag disabled={editable}>
+          <option>Tunissian bar association</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
+          <option>Option 5</option>
+        </Tag>
       </TagsContainer>
       <TagsContainer>
         <Title>Counties</Title>
-        <Tag>Tunisia</Tag>
+        <Tag disabled={editable}>
+          <option>Tunisia</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
+          <option>Option 5</option>
+        </Tag>
       </TagsContainer>
     </Wrapper>
   );
